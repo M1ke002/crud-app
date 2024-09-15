6e8c574f4c2bcfca4a7e7ee8b01fa6111d212ecf
M1ke002 <hoang.n.minh@gmail.com>
2024-09-15 13:44:38 +1000

    Fix small bug in controller

--- backend/src/controller/blog.ts
+++ backend/src/controller/blog.ts
@@ -4,7 +4,7 @@ import { Blog } from "../models";
 export const getAllBlogs = async (req: Request, res: Response) => {
   try {
     const blogs = await Blog.findAll();
-    res.status(200).json(blogs);
+    return res.status(200).json(blogs);
   } catch (error) {
     res.status(500).json({ error: "Error fetching blogs" });
   }
@@ -12,24 +12,29 @@ export const getAllBlogs = async (req: Request, res: Response) => {
 
 export const getBlog = async (req: Request, res: Response) => {
   const { id } = req.params;
+
   try {
     const blog = await Blog.findByPk(id);
     if (blog) {
-      res.status(200).json(blog);
+      return res.status(200).json(blog);
     } else {
-      res.status(404).json({ error: "Blog not found" });
+      return res.status(404).json({ error: "Blog not found" });
     }
   } catch (error) {
     res.status(500).json({ error: "Error fetching blog" });
   }
-  res.send({});
 };
 
 export const createBlog = async (req: Request, res: Response) => {
   const { title, content } = req.body;
+
+  if (!title) {
+    return res.status(404).json({ error: "Missing title" });
+  }
+
   try {
     const blog = await Blog.create({ title, content });
-    res.status(200).json(blog);
+    return res.status(200).json(blog);
   } catch (error) {
     res.status(500).json({ error: "Error creating blog" });
   }
@@ -38,15 +43,20 @@ export const createBlog = async (req: Request, res: Response) => {
 export const editBlog = async (req: Request, res: Response) => {
   const { id } = req.params;
   const { title, content } = req.body;
+
+  if (!title) {
+    return res.status(404).json({ error: "Missing data" });
+  }
+
   try {
     const blog = await Blog.findByPk(id);
     if (blog) {
       blog.title = title;
       blog.content = content;
       await blog.save();
-      res.status(200).json(blog);
+      return res.status(200).json(blog);
     } else {
-      res.status(404).json({ error: "Blog not found" });
+      return res.status(404).json({ error: "Blog not found" });
     }
   } catch (error) {
     res.status(500).json({ error: "Error editing blog" });
@@ -55,13 +65,14 @@ export const editBlog = async (req: Request, res: Response) => {
 
 export const deleteBlog = async (req: Request, res: Response) => {
   const { id } = req.params;
+
   try {
     const blog = await Blog.findByPk(id);
     if (blog) {
       await blog.destroy();
-      res.status(200).json("Blog deleted successfully!");
+      return res.status(200).json("Blog deleted successfully!");
     } else {
-      res.status(404).json({ error: "Blog not found" });
+      return res.status(404).json({ error: "Blog not found" });
     }
   } catch (error) {
     res.status(500).json({ error: "Error deleting blog" });
